62cf954134f41007ce6a8971a7a08eb71ea90415
DSingh <human.divanshu@gmail.com>
2017-02-20 05:58:38 +0530

    Completed water jug problem code

--- ai/waterjug.py
+++ ai/waterjug.py
@@ -1,54 +1,139 @@
-storage = []
+l = []
 
-def push_stack(state):
-    global stack
-    stack.append(state)
+def stackpush(s):
+	l.append(s)
+	
+def stackpop():
+	return l.pop()
 
-def pop_stack():
-    global stack
-    value = stack.pop()
-    return value
-    
+def stackempty():
+	return len(l) == 0
 
-jug1_size = 4
-jug2_size = 3
+def queueempty():
+	return len(l) == 0
 
-# in jug 2 water 2 L
-final_state = [2, 2]
+def queue(s):
+	l.append(s)
+	
+def dequeue():
+	global l
+	item = l[0]
+	l = l[1:]
+	return item
 
-def check_final(cur_state, final_state):
-    if cur_state[final_state[0] - 1] == final_state[1]:
-        return True
-    else:
-        return False
+def isfinal(indexofs):
+	j1 = indexofs / (jug2size + 1)
+	j2 = indexofs % (jug2size + 1)
+	return (j1 == finalwater) or (j2 == finalwater)
 
-current_state = [0,0]
+def decodestate(indexofs):
+	j1 = indexofs / (jug2size + 1)
+	j2 = indexofs % (jug2size + 1)
+	return j1, j2
 
-def generate_state(current):
-    global jug1_size
-    global jug2_size
-    jug1 = current[0]
-    jug2 = current[1]
-    
-    # fil jug 1
-    if jug1 < jug1_size:
-        jug1 = jug1_size
-        
-        
+def codestate(j1, j2):
+	return (jug2size + 1) * j1 + j2
 
-#jug1 = int(raw_input("Enter size of first jug:"))
-#jug2 = int(raw_input("Enter size of second jug:"))
-#jug = int(raw_input("Enter which you want water in the end ? (1 or 2) : "))
-#water = int(raw_input("How mush water you want in the end ? : "))
+def isvisited(indexofs):
+	return searchspace[indexofs][0] == 1
+	
 
-initial_state = [0,0]
-final_state = [jug, water]
+def addstate(j1, j2, parent):
+	stateindex = codestate(j1, j2)
+	if searchspace[stateindex][0] == 1:
+		print "Ignoring " + str(j1) + ", " + str(j2)
+	else:
+		print "Adding : " + str(j1) + ", " + str(j2)
+		searchspace[stateindex] = [1, parent]
+		l.append(codestate(j1, j2))
 
-# [[state], parent]
-search_space = []
+def generatestate(indexofs):
+	j1, j2 = decodestate(indexofs)
+	#empty 1
+	if j1 > 0:
+		print "Empty 1"
+		addstate(0, j2, indexofs)
+	#empty 2
+	if j2 > 0:
+		print "Empty 2"
+		addstate(j1, 0, indexofs)
+	# pour j1 to j2
+	if j1 != 0:
+		print "Pour j1 to j2"
+		topour = min(j1, jug2size - j2)
+		newj1 = j1 - topour
+		newj2 = j2 + topour
+		addstate(newj1, newj2, indexofs)
+	# pour j2 to j1
+	if j2 != 0:
+		print "j2 to j1"
+		topour = min(j2, jug1size - j1)
+		newj2 = j2 - topour
+		newj1 = j1 + topour
+		addstate(newj1, newj2, indexofs)
+	# fill j1
+	if j1 < jug1size:
+		print "fill j1"
+		addstate(jug1size, j2, indexofs)
+	# fill j2
+	if j2 < jug2size:
+		print "fill j2"
+		addstate(j1, jug2size, indexofs)
+	
+	
 
-current_state = initial_state
-# push initial state to stack
+def backtrack(indexofs):
+	cur = indexofs
+	ans = []
+	while cur != 0:
+		j1, j2 = decodestate(cur)
+		ans.append([j1, j2])
+		cur = searchspace[cur][1]
+	ans.append([0,0])
+	ans.reverse()
+	print ans
 
-while current_state != final_state:
-    
+def bfs():
+	flag = True
+	while flag or len(l) == 0:
+		tstate = dequeue()
+		print "Processing : state : " + str(tstate) + " : " + str(decodestate(tstate))
+		if isfinal(tstate):
+			backtrack(tstate)
+			flag = False
+		else:
+			generatestate(tstate)
+	if flag:
+		print "No possible solution"
+		
+	
+#jug1size = 5
+#jug2size = 3
+#finalwater = 4
+
+jug1size = int(raw_input("Enter size of jug 1:"))
+jug2size = int(raw_input("Enter size of jug 2:"))
+finalwater = int(raw_input("Enter amount of water in final state :"))
+
+
+# ask to choose algo here
+algo = 0 # for bfs - queue implementation
+
+searchspace = [] # [x, y] as item where x will tell if already generated or not
+# and y will tell about parent state
+
+
+for i in range(0, ((jug1size + 1) * (jug2size + 1))):
+	searchspace.append([0,0])
+searchspace[0] = [1,-1]	# because no one is parent of initial state
+
+#print searchspace
+
+istate = 0 # initial state
+
+if algo == 0:
+	queue(istate)
+	bfs()
+else:
+	stackpush(istate)
+	dfs()
